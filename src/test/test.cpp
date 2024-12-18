48f57dce43ca3531b05d171fa40caf66c992302e
zhangjunyi <58250587+junyiha@users.noreply.github.com>
2024-12-18 11:02:31 +0800

    整理源代码，增加测试源代码

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,28 +1,4 @@
-﻿/**
- * @file main.cpp
- * @author your name (you@domain.com)
- * @brief 碰钉机器人主函数
- * @version 0.1
- * @date 2024-09-13
- *
- * @copyright Copyright (c) 2024
- *
- */
-#include <QApplication>
-#include <string>
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/sinks/rotating_file_sink.h>
-#include <QThread>
-#include <QTextCodec>
-#include <clocale>
-#include <Eigen/Eigen>
-
-#include "HMI/SceneSelectionWindow.hpp"
-#include "vision/VisionInterface.h"
-#include "com/LaserDistanceBojke.h"
-#include "cxxopts.hpp"
+#include "test.hpp"
 
 int line_detect_demo(int argc, char* argv[])
 {
@@ -70,15 +46,6 @@ int laserDemo(int argc, char* argv[])
 
     QThread::sleep(3000 * 10);
     return 0;
-}
-
-int RunRobot(int argc, char* argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.setWindowTitle("LNG Panel Loading Robot");
-    w.show();
-    return a.exec();
 }
 
 int TestCom(int argc, char* argv[])
@@ -153,7 +120,6 @@ int TestInitLog(int argc, char* argv[])
     return 0;
 }
 
-
 int TestConfigManager(int argc, char* argv[])
 {
     TestInitLog(argc, argv);
@@ -167,51 +133,4 @@ int TestConfigManager(int argc, char* argv[])
         std::cerr << "\n";
     }
     return 0;
-}
-
-int RunSceneSelect(int argc, char* argv[])
-{
-    QApplication app(argc, argv);
-
-    APP::SceneSelectionWindow scene_selection_window;
-    scene_selection_window.show();
-
-    return app.exec();
-}
-
-int main(int argc, char* argv[])
-{
-    std::map<std::string, std::function<int(int, char**)>> FunctionMap =
-    {
-        {"robot", RunRobot},
-        {"line_demo", line_detect_demo},
-        {"laser_demo", laserDemo},
-        {"test_com", TestCom},
-        {"TestTask", TestTask},
-        {"TestConfigManager", TestConfigManager},
-        {"RunSceneSelect", RunSceneSelect}
-    };
-    cxxopts::Options options("Robot", "zbrobot's project");
-    options.add_options()("m,mode", "mode", cxxopts::value<std::string>()->default_value("robot"));
-    std::string mode{ "robot" };
-    try
-    {
-        auto result = options.parse(argc, argv);
-        mode = result["mode"].as<std::string>();
-    }
-    catch (...)
-    {
-        std::cerr << "parse argument failed\n";
-        return -1;
-    }
-    auto it = FunctionMap.find(mode);
-    if (it != FunctionMap.end())
-    {
-        it->second(argc, argv);
-    }
-    else
-    {
-        std::cerr << "invalid argument: " << mode << "\n";
-        return -1;
-    }
 }
---
+++ src/test/test.hpp
@@ -0,0 +1,45 @@
+/**
+ * @file test.hpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-12-18
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#pragma once
+#ifndef TEST_HPP__
+#define TEST_HPP__
+
+#include <QApplication>
+#include <string>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <QThread>
+#include <QTextCodec>
+#include <clocale>
+#include <Eigen/Eigen>
+
+#include "cxxopts.hpp"
+
+#include "HMI/SceneSelectionWindow.hpp"
+#include "vision/VisionInterface.h"
+#include "com/LaserDistanceBojke.h"
+
+int line_detect_demo(int argc, char* argv[]);
+
+int laserDemo(int argc, char* argv[]);
+
+int TestCom(int argc, char* argv[]);
+
+int TestTask(int argc, char* argv[]);
+
+int TestInitLog(int argc, char* argv[]);
+
+int TestConfigManager(int argc, char* argv[]);
+
+
+#endif  // TEST_HPP__
